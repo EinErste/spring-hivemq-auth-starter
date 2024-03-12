17b596cec552a82b90e616ce32dc53e3d222090e
Serhii <37027381+EinErste@users.noreply.github.com>
2024-03-12 21:14:02 +0200

    Change

--- src/main/java/org/ein/erste/iot/hivemq/auth/starter/service/device/init/IoTDeviceService.java
+++ src/main/java/org/ein/erste/iot/hivemq/auth/starter/service/device/init/IoTDeviceService.java
@@ -7,25 +7,27 @@ import org.ein.erste.iot.hivemq.auth.starter.domain.IoTDeviceSecret;
 import org.ein.erste.iot.hivemq.auth.starter.service.crypt.MessageCrypt;
 import org.ein.erste.iot.hivemq.auth.starter.service.device.crud.IoTDeviceCrudService;
 import org.ein.erste.iot.hivemq.auth.starter.settings.config.HiveMQServerCredentialsConfig;
-import org.ein.erste.iot.hivemq.auth.starter.settings.config.NginxServerConfig;
 import org.ein.erste.iot.hivemq.auth.starter.utils.DeviceAuthorizeResponse;
 import org.ein.erste.iot.hivemq.auth.starter.utils.DeviceCertificate;
 import org.ein.erste.iot.hivemq.auth.starter.utils.errors.ForbiddenException;
+import org.springframework.stereotype.Service;
 
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.util.Date;
 import java.util.Set;
 import java.util.UUID;
 
 @RequiredArgsConstructor
-public abstract class IoTDeviceService {
+@Service
+public class IoTDeviceService {
     private final IoTDeviceCrudService iotDeviceCrudService;
     private final MessageCrypt messageCrypt;
     private final HiveMQServerCredentialsConfig mqttCredentialsConfig;
-    private final NginxServerConfig nginxServerConfig;
 
     public DeviceAuthorizeResponse getCredentials(IoTDeviceSecret deviceSecret) {
         String serialNumber = messageCrypt.decrypt(deviceSecret.getKeyNumber(), deviceSecret.getSecret());
@@ -40,7 +42,7 @@ public abstract class IoTDeviceService {
         if (device.isBlacklisted()) throw new ForbiddenException();
 
         if (device.getCertificateExpiration().before(new Date())) {
-            var certificate = generateCertificateAndSend(serialNumber);
+            var certificate = generateCertificate(serialNumber);
             device.setCertificate(certificate.certificate());
             device.setCertificateExpiration(certificate.validTill());
             iotDeviceCrudService.save(device);
@@ -49,8 +51,8 @@ public abstract class IoTDeviceService {
         return device.getCertificate();
     }
 
-    public IoTDevice createDevice(String serialNumber){
-        var certificate = generateCertificateAndSend(serialNumber);
+    private IoTDevice createDevice(String serialNumber){
+        var certificate = generateCertificate(serialNumber);
         IoTDevice device = IoTDevice.builder()
                 .serialNumber(serialNumber)
                 .certificate(certificate.certificate())
@@ -65,25 +67,9 @@ public abstract class IoTDeviceService {
         return device;
     }
 
-    @SneakyThrows
-    public DeviceCertificate generateCertificateAndSend(String serialNumber) {
-        var certificate = generateCertificate(serialNumber);
-        var client = HttpClient.newHttpClient();
-
-        var request = HttpRequest.newBuilder(getURIAddCertificate(serialNumber))
-                .header("accept", "application/json")
-                .POST(HttpRequest.BodyPublishers.ofByteArray(certificate.certificate()))
-                .build();
-
-        var response = client.send(request, HttpResponse.BodyHandlers.ofString());
-
-        return certificate;
-    };
-
-    private URI getURIAddCertificate(String serialNumber) {
-        return URI.create(nginxServerConfig.getUrlAddCertificate() + "?api_key=" + nginxServerConfig.getAuthToken() + "&serial=" + serialNumber);
+    public DeviceCertificate generateCertificate(String serialNumber) {
+        var validTill = Date.from(LocalDateTime.now().plusDays(90).toInstant(ZoneOffset.UTC));
+        return new DeviceCertificate("stub_certificate".getBytes(), validTill);
     }
-
-    public abstract DeviceCertificate generateCertificate(String serialNumber);
 
 }
--- src/main/java/org/ein/erste/iot/hivemq/auth/starter/settings/GeneralConfiguration.java
+++ src/main/java/org/ein/erste/iot/hivemq/auth/starter/settings/GeneralConfiguration.java
@@ -1,17 +1,11 @@
 package org.ein.erste.iot.hivemq.auth.starter.settings;
 
 import org.ein.erste.iot.hivemq.auth.starter.settings.config.HiveMQServerCredentialsConfig;
-import org.ein.erste.iot.hivemq.auth.starter.settings.config.NginxServerConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class GeneralConfiguration {
-    @Bean
-    public NginxServerConfig stubNginxServerConfig() {
-        throw new IllegalStateException("NginxServerConfig bean is not provided");
-    }
-
     @Bean
     public HiveMQServerCredentialsConfig stubHiveMQServerCredentialsConfig() {
         throw new IllegalStateException("HiveMQServerCredentialsConfig bean is not provided");
